48c5531486e39a114414c9789b5cc7599cd6a595
Vytautas Narkevicius <vytautas.narkevicius@wisemonks.com>
2019-02-05 15:29:05 +0200

    make_it_work

--- lib/m.rb
+++ lib/m.rb
@@ -1,6 +1,20 @@
-require "m/version"
+# require "m/version"
 
-module M
-  class Error < StandardError; end
-  # Your code goes here...
+# module M
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
+
+Object.class_eval do
+
+  def M?(*args)
+    trace = "...#{/.*(\/.*).*(\/.*):in.*/.match(caller.first).captures.join}"
+    info_str = "[#{Time.now.strftime("%F %T")}] #{trace}"
+
+    open("#{Rails.root.to_s}/log/m.log", 'a') do |file|
+      file.puts info_str
+      file.puts args
+    end
+  end
+
 end
